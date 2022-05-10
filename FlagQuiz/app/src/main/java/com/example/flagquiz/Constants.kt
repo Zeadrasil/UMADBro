991077a6ea7efcc618426a157b37bf4c37033611
Zeadrasil <73188667+Zeadrasil@users.noreply.github.com>
2022-05-10 14:59:19 -0500

    Questions
    changed questions class and started adding in the list of questions that can be done

--- FlagQuiz/app/src/main/java/com/example/flagquiz/Constants.kt
+++ FlagQuiz/app/src/main/java/com/example/flagquiz/Constants.kt
@@ -8,136 +8,64 @@ object Constants {
     fun getQuestions(): ArrayList<Question>{
         val questionsList = ArrayList<Question>()
 
-        //Argentina Flag
-        var que = Question(
-            0,
-            "Which country does this flag represent?",
-            R.drawable.ic_flag_argentina,
-            "Argentina",
-            "Australia",
-            "Fiji",
-            "India",
-            1
-        )
+        var que = Question(0, "Use food for stress relief?", 10, -5, -1, -1)
         questionsList.add(que)
 
-        //Aus Flag
-        que = Question(
-            1,
-            "Which country does this flag represent?",
-            R.drawable.ic_flag_australia,
-            "Great Britain",
-            "Kuwait",
-            "Australia",
-            "Argentina",
-            3
-        )
+        que = Question(1, "Get into a brawl with the person stalking you?", 20, -10, -1, -1)
         questionsList.add(que)
 
-        //Belgium Flag
-        que = Question(
-            2,
-            "Which country does this flag represent?",
-            R.drawable.ic_flag_belgium,
-            "Germany",
-            "Australia",
-            "Fiji",
-            "Belgium",
-            4
-        )
+        que = Question(2, "Take the day off work?", 10, -1, -5, -1)
         questionsList.add(que)
 
-        //Brazil Flag
-        que = Question(
-            1,
-            "Which country does this flag represent?",
-            R.drawable.ic_flag_brazil,
-            "Argentina",
-            "Kuwait",
-            "India",
-            "Brazil",
-            4
-        )
+        que = Question(3, "Get therapy?", 20, -1, -10, -1)
         questionsList.add(que)
 
-        //Denmark Flag
-        que = Question(
-            1,
-            "Which country does this flag represent?",
-            R.drawable.ic_flag_denmark,
-            "Switzerland",
-            "Denmark",
-            "Germany",
-            "New Zealand",
-            2
-        )
+        que = Question(4, "Yarhar a self help book?", 10, -1, -1, -5)
         questionsList.add(que)
 
-        //fiji Flag
-        que = Question(
-            1,
-            "Which country does this flag represent?",
-            R.drawable.ic_flag_fiji,
-            "Fiji",
-            "Australia",
-            "England",
-            "New Zealand",
-            1
-        )
+        que = Question(5, "Use magic to transfer your negative emotions to another person?",
+            20, -1, -1, -10)
         questionsList.add(que)
 
-        //Germany Flag
-        que = Question(
-            1,
-            "Which country does this flag represent?",
-            R.drawable.ic_flag_germany,
-            "Switzerland",
-            "Belgium",
-            "Germany",
-            "Australia",
-            3
-        )
+        que = Question(6, "Work out instead of going out with friends?", -5, 10, -1, -1)
         questionsList.add(que)
 
-        //India Flag
-        que = Question(
-            1,
-            "Which country does this flag represent?",
-            R.drawable.ic_flag_india,
-            "Fiji",
-            "India",
-            "Kuwait",
-            "Philippines",
-            2
-        )
+        que = Question(7, "Go through boot camp?", -10, 20, -1, -1)
         questionsList.add(que)
 
-        //Kuwait Flag
-        que = Question(
-            1,
-            "Which country does this flag represent?",
-            R.drawable.ic_flag_kuwait,
-            "Kuwait",
-            "Italy",
-            "Greece",
-            "Turkey",
-            1
-        )
+        que = Question(8, "Get a checkup at the doctor?", -1, 10, -5, -1)
         questionsList.add(que)
 
-        //NZ Flag
-        que = Question(
-            1,
-            "Which country does this flag represent?",
-            R.drawable.ic_flag_new_zealand,
-            "South Africa",
-            "Australia",
-            "New Zealand",
-            "England",
-            3
-        )
+        que = Question(9, "Go to the hospital?", -1, 20, -10, -1)
         questionsList.add(que)
 
-            return questionsList
+        que = Question(10, "Steal medicine from an old lady?", -1, 10, -1, -5)
+        questionsList.add(que)
+
+        que = Question(11, "Use magic to steal the life force of another person?",
+            -1, 20, -1, -10)
+        questionsList.add(que)
+
+        que = Question(12, "Work overtime?", -5, -1, 10, -1)
+        questionsList.add(que)
+
+        que = Question(13, "Get a part-time customer service job?", -10, -1, 20, -1)
+        questionsList.add(que)
+
+        que = Question(14, "Move some heavy boxes for a friend that is moving?", -1, -5, 10, -1)
+        questionsList.add(que)
+
+        que = Question(15, "Sell one of your unnecessary organs?", -1, -10, 20, -1)
+        questionsList.add(que)
+
+        que = Question(16, "Mug somebody?", -1, -1, 10, -5)
+        questionsList.add(que)
+
+        que = Question(17, "Sell an NFT?", -1, -1, 20, -10)
+        questionsList.add(que)
+
+        que = Question(18, "Go on jury duty?", -5, -1, -1, 10)
+        questionsList.add(que)
+        return questionsList
     }
 }
